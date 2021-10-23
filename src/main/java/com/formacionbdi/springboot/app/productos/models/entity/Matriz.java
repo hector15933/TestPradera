9fe593680777a89f8996f85f02232d745353b3b5
hreyesgps <hector_12_96@outlook.es>
2021-10-22 23:14:46 -0500

    cloud

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Matriz.java
@@ -0,0 +1,34 @@
+package com.formacionbdi.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class Matriz implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Object matrizTres;
+	
+	
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+}
